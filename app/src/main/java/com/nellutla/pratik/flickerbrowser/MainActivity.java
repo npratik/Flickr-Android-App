cd8145cf3cf4fcc6bc0a18968aae362872d1b5d9
Pratik Nellutla <nellutla.p@husky.neu.edu>
2017-05-12 23:01:52 -0400

    Initial Commit
    Flickr App initial commit

---
+++ app/src/main/java/com/nellutla/pratik/flickerbrowser/MainActivity.java
@@ -0,0 +1,73 @@
+package com.nellutla.pratik.flickerbrowser;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d(TAG, "onCreate: starts");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        GetRawData getRawData = new GetRawData(this);
+        getRawData.execute("https://api.flickr.com/services/feeds/photos_public.gne?tags=android,nagout,sdk&tagmode=any&format=json&nojsoncallback=1");
+        Log.d(TAG, "onCreate: ends");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.d(TAG, "onCreateOptionsMenu() returned: " + true);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        Log.d(TAG, "onOptionsItemSelected() returned: returned ");
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public  void DownloadComplete(String data , DownloadStatus status){
+        if(status==DownloadStatus.OK){
+            Log.d(TAG, "DownloadComplete: data is " + data);
+        }else {
+            Log.e(TAG, "DownloadComplete: failed with status " + status );
+        }
+
+    }
+}
